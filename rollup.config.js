3f63c251ac4f98170bcf0a05c088a776385ff79d
Blaine Richardson <blainerrichardson@gmail.com>
2020-11-07 15:53:22 -0600

    chore: cleaning up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -48,7 +48,7 @@ export default {
 			},
 			preprocess: sveltePreprocess({
         postcss: {
-          includePaths: ['node_modules', 'theme'],
+          includePaths: ['theme'],
         }
       })
 		}),
